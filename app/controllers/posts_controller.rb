e7c18ea1ea1f59cc51a4ddf4dc2cc0609a7b74e7
Gamoundo <akilgg@gmail.com>
2021-10-26 12:50:29 -0400

    added index, fixed create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,20 @@
 class PostsController < ApplicationController
 
+    def index
+        posts=Post.all
+        render json: posts
+    end
+    
     def create
-    post=Post.create!(post_params)
+    post=Post.create!(caption: post_params["caption"] )
+    post.avatar.attach(post_params["photo"])
+    post.photo= url_for(post.avatar)
+
+        if post.save
+            render json: post
+        else
+            render json: post.errors
+        end
     
     end
 
