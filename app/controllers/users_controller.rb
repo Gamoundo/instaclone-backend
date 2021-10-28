e5e0dae813d0b1867ac4d2e701ef8db85e3c5f8d
Gamoundo <akilgg@gmail.com>
2021-10-28 01:24:20 -0400

    setup proper route for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
 
     def create
-    
+        user=User.create!(user_params)
+
+        if user
+            render json: user.to_json(:except => :password)
+        else
+            render json: "user already exists"
+        end
     end
+
+    def show
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password)
+    end
+
+    
 
 end
