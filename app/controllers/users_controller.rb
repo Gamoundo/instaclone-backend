ceb10ecf9370472a4fea0197b56b17484c57fa31
Gamoundo <akilgg@gmail.com>
2021-11-02 19:11:15 -0400

    setup login and register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,25 @@ class UsersController < ApplicationController
         if user
             render json: user.to_json(:except => :password)
         else
-            render json: "user already exists"
+            render json: {error: "user already exists"}
         end
     end
 
-    def show
+    def login
+        user = User.find_by(name: user_params[:name])
 
+        if user && user.authenticate(user_params[:password])
+            # token = encode_token({user_id: @user.id})
+            # render json: {user: @user, token: token}
+            render json: user.to_json(:except => :password)
+        else
+             render json: {error: "Insufficient credentials"} 
+        end
+    end
+
+    def index
+        users = User.all
+        render json: users
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
   post "/signup", to: "users#create"
+  post "login", to: "users#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
